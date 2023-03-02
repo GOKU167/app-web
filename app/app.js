cdb33d672f4f4a9551aab4d40e3406a2b12669d0
Fardeen <87431360+GOKU167@users.noreply.github.com>
2023-03-02 21:05:29 +0530

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,9 @@
+let guw = Math.floor(Math.random() * 101);
+let geh = document.getElementsByClassName("d21-1")[0];
+async function htht() {
+  setTimeout(() => {
+    geh.innerHTML = guw;
+  }, 1000);
+}
+htht();
+
